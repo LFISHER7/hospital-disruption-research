25edb0d3c595f5dfdabaea2414e626a16e79e377
Brian <brianmkcebm@gmail.com>
2021-02-01 14:41:11 +0000

    imd quintiles

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -83,6 +83,36 @@ study = StudyDefinition(
         },
     ),
 
+    # IMD - quintile
+    imd=patients.categorised_as(
+        {
+            "0": "DEFAULT", 
+            "1": """index_of_multiple_deprivation >=1 AND index_of_multiple_deprivation < 32844*1/5""",
+            "2": """index_of_multiple_deprivation >= 32844*1/5 AND index_of_multiple_deprivation < 32844*2/5""",
+            "3": """index_of_multiple_deprivation >= 32844*2/5 AND index_of_multiple_deprivation < 32844*3/5""",
+            "4": """index_of_multiple_deprivation >= 32844*3/5 AND index_of_multiple_deprivation < 32844*4/5""",
+            "5": """index_of_multiple_deprivation >= 32844*4/5 AND index_of_multiple_deprivation < 32844""",
+        },
+        index_of_multiple_deprivation=patients.address_as_of(
+            campaign_start,
+            returning="index_of_multiple_deprivation",
+            round_to_nearest=100,
+        ),
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "0": 0.05,
+                    "1": 0.19,
+                    "2": 0.19,
+                    "3": 0.19,
+                    "4": 0.19,
+                    "5": 0.19,
+                }
+            },
+        },
+    ),
+
 
 
     CVD=patients.admitted_to_hospital(
